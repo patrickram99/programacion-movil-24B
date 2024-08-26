b49273efb068ba3ab4e5620f298b6fba333539a0
Patrick Ramirez <pramirezs@ulasalle.edu.pe>
2024-08-26 07:24:53 -0500

    Añadir E2
    Se añaden funciones playerWins, playerLoses y getRandomOption para optener ganador, perdedor e input de usuario respectivamente

---
+++ U1_Introduccion_Kotlin/src/main/kotlin/P1_Inroducción_Kotlin/E2_PiedraPapelTijera.kt
@@ -0,0 +1,50 @@
+package p1IntroduccionKotlin
+
+// Definimos las opciones del juego como constantes (val)
+val options = mapOf(
+    "Piedra" to mapOf("wins" to "Tijeras", "loses" to "Papel"),
+    "Papel" to mapOf("wins" to "Piedra", "loses" to "Tijeras"),
+    "Tijeras" to mapOf("wins" to "Papel", "loses" to "Piedra")
+)
+
+val values = listOf("Piedra", "Papel", "Tijeras")
+
+fun main(args: Array<String>) {
+    val computerOption = getRandomOption()
+    val input = getUserInput()
+
+    when {
+        playerWins(input, computerOption) -> println("Has GANADO, jugaste $input contra $computerOption")
+        playerLoses(input, computerOption) -> println("Has PERDIDO, jugaste $input contra $computerOption")
+        else -> println("Es un EMPATE, ambos jugaron $input")
+    }
+}
+
+// Función para obtener una opción aleatoria
+fun getRandomOption(): String {
+    return values.random()
+}
+
+// Función para verificar si el usuario gana
+fun playerWins(playerOption: String, computerOption: String): Boolean {
+    return computerOption == options[playerOption]?.get("wins")
+}
+
+// Función para verificar si el usuario pierde
+fun playerLoses(playerOption: String, computerOption: String): Boolean {
+    return computerOption == options[playerOption]?.get("loses")
+}
+
+// Función para solicitar y validar la entrada del usuario
+fun getUserInput(): String {
+    while (true) {
+        println("Por favor, escribe tu jugada (Piedra, Papel o Tijeras):")
+        val input = readln()
+
+        if (input in values) {
+            return input
+        } else {
+            println("Entrada no válida. Intenta de nuevo.")
+        }
+    }
+}
