a3bd89a8c2c34a9ff2f41d8e122cf7f70d7045cb
Patrick Ramirez <pramirezs@ulasalle.edu.pe>
2024-09-20 21:11:55 -0500

    P3-E2 Añadir reproduccion automatica al finalizar una cancion

--- U1_Practica_3/app/src/main/java/com/example/practica3/E2_MusicApp/MusicApp.kt
+++ U1_Practica_3/app/src/main/java/com/example/practica3/E2_MusicApp/MusicApp.kt
@@ -9,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.practica3.R
 
 class MusicApp : AppCompatActivity() {
-    private lateinit var music: MediaPlayer
+    private var music: MediaPlayer? = null
     private lateinit var playOrPauseButton: ImageButton
     private lateinit var nextSongButton: ImageButton
     private lateinit var previousSongButton: ImageButton
@@ -17,6 +17,12 @@ class MusicApp : AppCompatActivity() {
     private lateinit var artistName: TextView
     private lateinit var albumArt: ImageView
     private var currentSong: Int = 0
+
+    private val songs: Array<String> by lazy { resources.getStringArray(R.array.songs) }
+    private val artists: Array<String> by lazy { resources.getStringArray(R.array.artists) }
+
+    private val albumArts = arrayOf(R.drawable.emotion, R.drawable.ts1989, R.drawable.lullaby)
+    private val audio = arrayOf(R.raw.emotion, R.raw.new_romantics, R.raw.wth)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,30 +35,55 @@ class MusicApp : AppCompatActivity() {
         artistName = findViewById(R.id.artist_name)
         albumArt = findViewById(R.id.album_art)
 
-        val songs = resources.getStringArray(R.array.songs)
-        val artists = resources.getStringArray(R.array.artists)
-        val albumArts = arrayOf(R.drawable.emotion, R.drawable.ts1989, R.drawable.lullaby)
-
-        updateUI(songs, artists, albumArts)
+        updateUI()
 
         nextSongButton.setOnClickListener {
             if (currentSong < songs.size - 1) {
                 currentSong += 1
-                updateUI(songs, artists, albumArts)
+                updateUI()
+                music?.start()
             }
         }
 
         previousSongButton.setOnClickListener {
             if (currentSong > 0) {
                 currentSong -= 1
-                updateUI(songs, artists, albumArts)
+                updateUI()
+                music?.start()
+            }
+        }
+
+        playOrPauseButton.setOnClickListener {
+            music?.let {
+                if (it.isPlaying) {
+                    it.pause()
+                    playOrPauseButton.setImageResource(R.drawable.play)
+                } else {
+                    it.start()
+                    playOrPauseButton.setImageResource(R.drawable.pause)
+                }
             }
         }
     }
 
-    private fun updateUI(songs: Array<String>, artists: Array<String>, albumArts: Array<Int>) {
+    private fun updateUI() {
         songTitle.text = songs[currentSong]
         artistName.text = artists[currentSong]
         albumArt.setImageResource(albumArts[currentSong])
+        music?.release()
+        music = MediaPlayer.create(this, audio[currentSong])
+        music?.setVolume(0.5f, 0.5f)
+        music?.start()
+        playOrPauseButton.setImageResource(R.drawable.pause)
+        music?.setOnCompletionListener {
+            if (currentSong < songs.size - 1) {
+                currentSong += 1
+                updateUI()
+                music?.start()
+            } else {
+                playOrPauseButton.setImageResource(R.drawable.play)
+            }
+        }
     }
+
 }
